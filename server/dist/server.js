35876a721c4e788294a6877ac9356982fee38094
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2020-02-01 17:28:52 +0530

    homepage completed

--- server/dist/server.js
+++ server/dist/server.js
@@ -1,5 +1,7 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
@@ -7,14 +9,14 @@ const bearerToken = require("express-bearer-token");
 const product_1 = require("./product");
 const auth_1 = require("./auth");
 const app = express()
-    .use(cors())
-    .use(bodyParser.json())
-    .use(bearerToken())
-    .use(auth_1.oktaAuth)
-    .use(product_1.router);
+  .use(cors())
+  .use(bodyParser.json())
+  .use(bearerToken())
+  .use(auth_1.oktaAuth)
+  .use(product_1.router);
 app.listen(4201, err => {
-    if (err) {
-        return console.log(err);
-    }
-    return console.log("My Node App listening on port 4201");
+  if (err) {
+    return console.log(err);
+  }
+  return console.log("My Node App listening on port 4201");
 });
